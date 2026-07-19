8115f92822bf39ee75c1e32eccf90ce14bfda5f4
tuananhtshop123-rgb <tuananhtshop123@gmail.com>
2026-07-19 21:48:12 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -8,44 +8,61 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let players = {};
+let rooms = {}; // Quản lý phòng
+let chatHistory = []; 
 
 io.on('connection', (socket) => {
+    // Đăng nhập
     socket.on('join', (data) => {
-        players[socket.id] = {
-            id: socket.id,
-            name: data.name,
-            color: data.color,
-            x: 400, y: 300,
-            chat: ""
-        };
-        io.emit('update', players);
+        socket.playerName = data.name;
+        socket.playerColor = data.color;
+        // Mặc định vào sảnh chờ, chưa vào phòng
     });
 
-    socket.on('move', (pos) => {
-        if (players[socket.id]) {
-            players[socket.id].x = pos.x;
-            players[socket.id].y = pos.y;
-            io.emit('update', players);
+    // Tạo phòng
+    socket.on('createRoom', () => {
+        const code = Math.floor(10000 + Math.random() * 90000).toString();
+        rooms[code] = { players: {}, zombies: [], wave: 1 };
+        socket.join(code);
+        socket.roomId = code;
+        rooms[code].players[socket.id] = { id: socket.id, name: socket.playerName, color: socket.playerColor, x: 400, y: 300, chat: "" };
+        socket.emit('roomCreated', code);
+    });
+
+    // Tham gia phòng
+    socket.on('joinRoom', (code) => {
+        if(rooms[code]) {
+            socket.join(code);
+            socket.roomId = code;
+            rooms[code].players[socket.id] = { id: socket.id, name: socket.playerName, color: socket.playerColor, x: 400, y: 300, chat: "" };
+            io.to(code).emit('update', rooms[code].players);
         }
     });
 
+    // Di chuyển
+    socket.on('move', (pos) => {
+        if(socket.roomId && rooms[socket.roomId] && rooms[socket.roomId].players[socket.id]) {
+            rooms[socket.roomId].players[socket.id].x = pos.x;
+            rooms[socket.roomId].players[socket.id].y = pos.y;
+            io.to(socket.roomId).emit('update', rooms[socket.roomId].players);
+        }
+    });
+
+    // Chat
     socket.on('chat', (msg) => {
-        if (players[socket.id]) {
-            players[socket.id].chat = msg;
-            io.emit('update', players);
-            // Tự xóa tin nhắn sau 3 giây
-            setTimeout(() => {
-                if(players[socket.id]) players[socket.id].chat = "";
-                io.emit('update', players);
-            }, 3000);
+        if(socket.roomId) {
+            const entry = { name: socket.playerName, msg: msg };
+            chatHistory.push(entry);
+            io.to(socket.roomId).emit('newChat', entry);
         }
     });
 
     socket.on('disconnect', () => {
-        delete players[socket.id];
-        io.emit('update', players);
+        if(socket.roomId && rooms[socket.roomId]) {
+            delete rooms[socket.roomId].players[socket.id];
+            io.to(socket.roomId).emit('update', rooms[socket.roomId].players);
+        }
     });
 });
 
-server.listen(3000, () => console.log('Server game đã chạy!'));
+server.listen(3000, () => console.log('Server chạy cổng 3000'));
